30a258550e49067d1666dcd12ff87e8e45bb3f01
Lachlan Stephan <ljstephan116@gmail.com>
2021-03-08 20:47:50 +1000

    regUser route/query working from client POST

--- models/db.js
+++ models/db.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 require("dotenv").config({ path: "./config/.env" });
 
+// DB connection
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -10,6 +11,7 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+// Test func for * users
 const getUsers = (res) => {
   connection.query("SELECT * FROM Users", (err, results) => {
     if (!err) {
@@ -20,6 +22,7 @@ const getUsers = (res) => {
   });
 };
 
+// Func to retrieve schedule for users
 const getSchedule = (res) => {
   connection.query("SELECT * FROM Schedule", (err, results) => {
     if (!err) {
@@ -30,18 +33,30 @@ const getSchedule = (res) => {
   });
 };
 
-const regUser = (res) => {
-  let sql =
-    "INSERT INTO Users (fName, email, password) VALUES ('fName', 'lName', 'email', 'password')";
-  connection.query(sql, (err, rows) => {
-    if (!err) {
-      res.send(results);
-    } else {
-      res.status(400);
+// Func to register user details
+const regUser = (req, res) => {
+  // set form data
+  let values = [
+    req.body.fName,
+    req.body.lName,
+    req.body.email,
+    req.body.password,
+  ];
+  // insert reg details to DB
+  connection.query(
+    "INSERT INTO Users (fName, lName, email, password) VALUES (?, ?, ?, ?)",
+    values,
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(rows);
+      }
     }
-  });
+  );
 };
 
+// Exports all func
 module.exports = {
   connection,
   getUsers,
--- server.js
+++ server.js
@@ -3,25 +3,33 @@ const app = express();
 const port = 5000;
 const cors = require("cors");
 const nodemon = require("nodemon");
-// const connection = require("./models/db");
+const bodyParser = require("body-parser");
 const dbFunc = require("./models/db");
 
+// Handle cors errors
 app.use(cors());
+
+let jsonParser = bodyParser.json();
 
 app.get("/", (req, res) => {
   res.send("Home");
 });
 
+// Test route for all users`
 app.get("/users", (req, res) => {
   dbFunc.getUsers(res);
 });
 
+// Retrieve schedule for user
 app.get("/schedule", (req, res) => {
   dbFunc.getSchedule(res);
 });
 
-app.post("/regUser", (req, res) => {
-  dbFunc.regUser(res);
+// Post register details
+app.post("/regUser", jsonParser, (req, res) => {
+  console.log("got body", req.body);
+  res.sendStatus(200);
+  dbFunc.regUser(req);
 });
 
 app.listen(port, () => {
