560b8f769a73a097b456f0b674044aafa8c04a74
Lachlan Stephan <ljstephan116@gmail.com>
2021-03-11 12:42:35 +1000

    regUser checks existing user first now

--- models/db.js
+++ models/db.js
@@ -1,19 +1,23 @@
 const mysql = require("mysql");
+const { request, rawListeners } = require("../server");
 require("dotenv").config({ path: "./config/.env" });
-
+////////////////////////////////////////////////////////////
 // DB connection
-const connection = mysql.createConnection({
+////////////////////////////////////////////////////////////
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
 });
-connection.connect();
 
+////////////////////////////////////////////////////////////
 // Test func for * users
+////////////////////////////////////////////////////////////
 const getUsers = (res) => {
-  connection.query("SELECT * FROM Users", (err, results) => {
+  let sql = "SELECT * FROM Users";
+  pool.query(sql, (err, results) => {
     if (!err) {
       res.send(results);
     } else {
@@ -21,45 +25,92 @@ const getUsers = (res) => {
     }
   });
 };
-
+////////////////////////////////////////////////////////////
 // Func to retrieve schedule for users
-const getSchedule = (res) => {
-  connection.query("SELECT * FROM Schedule", (err, results) => {
+////////////////////////////////////////////////////////////
+const getSchedule = (req, res) => {
+  let sql = "SELECT * FROM Schedule";
+  pool.query(sql, (err, rows) => {
     if (!err) {
-      res.send(results);
+      res.status(201).send(rows);
     } else {
-      res.status(400);
+      res.send(204);
     }
   });
 };
 
+////////////////////////////////////////////////////////////
 // Func to register user details
+////////////////////////////////////////////////////////////
 const regUser = (req, res) => {
-  // set form data
+  // set form data that will be inserted
   let values = [
     req.body.fName,
     req.body.lName,
     req.body.email,
     req.body.password,
   ];
-  // insert reg details to DB
-  connection.query(
-    "INSERT INTO Users (fName, lName, email, password) VALUES (?, ?, ?, ?)",
-    values,
-    (err, rows) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(rows);
-      }
+
+  // SQL that will be used for insert to DB
+  let sql = "INSERT INTO Users (fName, lName, email, password) VALUES (?)";
+
+  // For checking the email exists query
+  let checkEmail = req.body.email;
+
+  // Checks if email is in use
+  let check_user_sql = "SELECT * FROM USERS WHERE email = (?)";
+
+  // Query to check the email (exists or not)
+  pool.query(check_user_sql, checkEmail, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send();
     }
-  );
+    if (rows.length > 0) {
+      console.log("email in use");
+      res.status(403).send();
+      pool.release;
+    } else {
+      // if new user ----> add new user
+      pool.query(sql, [values], (err, rows) => {
+        if (err) {
+          res.status(400).send();
+          console.log(err);
+        } else {
+          console.log("success");
+          res.status(201).send();
+        }
+      });
+    }
+  });
 };
 
+////////////////////////////////////////////////////////////
+// Func to check the login
+////////////////////////////////////////////////////////////
+const login = (req, res) => {
+  // set login data
+  let values = [req.body.email, req.body.password];
+  let sql = "SELECT * FROM Users where email = ? AND password = ?";
+  // run the query
+  pool.query(sql, [values], (err, rows) => {
+    if (rows.length > 0) {
+      // set id and isloggedin boolean in session
+      req.session.isloggedin = true;
+      req.session.userID = userID;
+      res.statusCode(200);
+    } else {
+      res.statusCode(401);
+    }
+  });
+};
+
+////////////////////////////////////////////////////////////
 // Exports all func
+////////////////////////////////////////////////////////////
 module.exports = {
-  connection,
   getUsers,
   getSchedule,
   regUser,
+  login,
 };
--- server.js
+++ server.js
@@ -5,9 +5,26 @@ const cors = require("cors");
 const nodemon = require("nodemon");
 const bodyParser = require("body-parser");
 const dbFunc = require("./models/db");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
-// Handle cors errors
+app.use(cookieParser());
+app.set("trust proxy", 1);
+app.use(
+  session({
+    name: "scheduleStuffCookie",
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+// Handle cors
 app.use(cors());
+// let corsOptions = {
+//   origin: "http://localhost:3000",
+//   optionsSuccessStatus: 204,
+// };
 
 let jsonParser = bodyParser.json();
 
@@ -22,16 +39,24 @@ app.get("/users", (req, res) => {
 
 // Retrieve schedule for user
 app.get("/schedule", (req, res) => {
-  dbFunc.getSchedule(res);
+  dbFunc.getSchedule(req, res);
 });
 
 // Post register details
 app.post("/regUser", jsonParser, (req, res) => {
   console.log("got body", req.body);
   res.sendStatus(200);
-  dbFunc.regUser(req);
+  dbFunc.regUser(req, res);
 });
 
+// Handle the login
+app.post("/login", jsonParser, (req, res) => {
+  console.log("got body", req.body);
+  res.sendStatus(200);
+  dbFunc.login(req, res);
+});
+
+// Start server on this port
 app.listen(port, () => {
   console.log(`listening at ${port} `);
 });
